6b860657322daff5076044e0ec731bd2dc86d4e9
LJ Johnson <MemnochDaCoder@users.noreply.github.com>
2023-09-10 21:02:24 -0600

    Add files via upload

---
+++ Controllers/BattleController.cs
@@ -0,0 +1,44 @@
+﻿using Lib.Repository.Entities;
+using Lib.Repository.Repository;
+using Lib.Repository.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+public class BattleController : BaseApiController
+{
+    private readonly IBattleOfMonstersRepository _repository;
+
+    public BattleController(IBattleOfMonstersRepository monRepo)
+    {
+        _repository = monRepo;
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> GetAll()
+    {
+        IEnumerable<Battle> battles = await _repository.Battles.GetAllAsync();
+        return Ok(battles);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> Add([FromBody] Battle battle)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> Remove(int id)
+    {
+        await _repository.Battles.RemoveAsync(id);
+        await _repository.Save();
+        return Ok();
+    }
+}
+
+
---
+++ Lib.ProblemSolving/Challenge2/Challenge2.cs
@@ -0,0 +1,9 @@
+namespace Lib.ProblemSolving;
+
+public static class Challenge2
+{
+    public static int DiceFacesCalculator(int dice1, int dice2, int dice3)
+    {
+        return 0;
+    }
+}
